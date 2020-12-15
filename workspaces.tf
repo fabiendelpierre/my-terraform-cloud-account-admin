0bc666b9fc8e1b107aa0caf3dd73a3cf5ca1810c
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2020-12-15 16:54:57 -0500

    add workspace for my Vault cluster

--- workspaces.tf
+++ workspaces.tf
@@ -12,3 +12,18 @@ resource "tfe_workspace" "azure_msdn_subscription" {
   allow_destroy_plan  = true
   auto_apply          = false
 }
+
+resource "tfe_workspace" "vault_configuration" {
+  name         = "my-vault-cluster-configuration"
+  organization = var.my_tfc_organization
+
+  terraform_version = "0.14.2"
+
+  vcs_repo {
+    identifier = data.terraform_remote_state.github_repositories.outputs.vault_configuration_repo.full_name
+    oauth_token_id = data.tfe_oauth_client.my_oauth_client.oauth_token_id
+  }
+
+  allow_destroy_plan  = true
+  auto_apply          = false
+}
