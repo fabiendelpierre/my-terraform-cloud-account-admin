b96bccd29a0515b3bf4efbfd61abc3522a6a2c1a
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2020-12-16 17:43:16 -0500

    this is supposed to be the correct syntax

--- module_registry.tf
+++ module_registry.tf
@@ -1,9 +1,9 @@
 resource "tfe_registry_module" "virtual_network" {
   vcs_repo {
-    # display_identifier = data.terraform_remote_state.github_repositories.outputs.module_azurerm_virtualnetwork_repo.full_name
-    display_identifier = "terraform-azurerm-virtualnetwork"
-    # identifier         = data.terraform_remote_state.github_repositories.outputs.module_azurerm_virtualnetwork_repo.full_name
-    identifier         = "fabiendelpierre/terraform-azurerm-virtualnetwork"
+    display_identifier = data.terraform_remote_state.github_repositories.outputs.module_azurerm_virtualnetwork_repo.full_name
+    # display_identifier = "fabiendelpierre/terraform-azurerm-virtualnetwork"
+    identifier         = data.terraform_remote_state.github_repositories.outputs.module_azurerm_virtualnetwork_repo.full_name
+    # identifier         = "fabiendelpierre/terraform-azurerm-virtualnetwork"
     oauth_token_id     = data.tfe_oauth_client.my_oauth_client.oauth_token_id
   }
 }
