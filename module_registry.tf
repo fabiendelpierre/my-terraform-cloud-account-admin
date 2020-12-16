f3435043935bd1f2d1b5b2470fdebd5dcca3dd19
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2020-12-16 17:13:51 -0500

    these aren't documented because they can't be set :|

--- module_registry.tf
+++ module_registry.tf
@@ -1,8 +1,4 @@
 resource "tfe_registry_module" "virtual_network" {
-  name = "virtualnetwork"
-  organization = var.my_tfc_organization
-  module_provider = "github"
-
   vcs_repo {
     display_identifier = data.terraform_remote_state.github_repositories.outputs.module_azurerm_virtualnetwork_repo.full_name
     identifier         = data.terraform_remote_state.github_repositories.outputs.module_azurerm_virtualnetwork_repo.full_name
