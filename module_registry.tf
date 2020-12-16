ab7ad825e5fc546d0daac75e7533c2bb89fba14d
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2020-12-16 17:16:09 -0500

    full path?

--- module_registry.tf
+++ module_registry.tf
@@ -3,7 +3,7 @@ resource "tfe_registry_module" "virtual_network" {
     # display_identifier = data.terraform_remote_state.github_repositories.outputs.module_azurerm_virtualnetwork_repo.full_name
     display_identifier = "terraform-azurerm-virtualnetwork"
     # identifier         = data.terraform_remote_state.github_repositories.outputs.module_azurerm_virtualnetwork_repo.full_name
-    identifier         = "terraform-azurerm-virtualnetwork"
+    identifier         = "fabiendelpierre/terraform-azurerm-virtualnetwork"
     oauth_token_id     = data.tfe_oauth_client.my_oauth_client.oauth_token_id
   }
 }
