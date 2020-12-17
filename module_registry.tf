0ef80a7c0c63ec1876fe3b9c3fee6398efb73088
Fabien Delpierre <fabien.delpierre@gmail.com>
2020-12-17 16:05:15 -0500

    cleanup

--- module_registry.tf
+++ module_registry.tf
@@ -2,9 +2,7 @@ resource "tfe_registry_module" "virtual_network" {
   provider = tfe.module_registry
   vcs_repo {
     display_identifier = data.terraform_remote_state.github_repositories.outputs.module_azurerm_virtualnetwork_repo.full_name
-    # display_identifier = "fabiendelpierre/terraform-azurerm-virtualnetwork"
     identifier         = data.terraform_remote_state.github_repositories.outputs.module_azurerm_virtualnetwork_repo.full_name
-    # identifier         = "fabiendelpierre/terraform-azurerm-virtualnetwork"
     oauth_token_id     = data.tfe_oauth_client.my_oauth_client.oauth_token_id
   }
 }
