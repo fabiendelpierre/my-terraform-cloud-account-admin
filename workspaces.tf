136c23d31732f1d027e6c93264a40cbd0f55e137
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2021-01-04 15:58:20 -0500

    update TF version used in workspace

--- workspaces.tf
+++ workspaces.tf
@@ -2,7 +2,7 @@ resource "tfe_workspace" "azure_msdn_subscription" {
   name         = "my-azure-msdn-subscription"
   organization = var.my_tfc_organization
 
-  terraform_version = "0.14.2"
+  terraform_version = "0.14.3"
 
   vcs_repo {
     identifier = data.terraform_remote_state.github_repositories.outputs.azure_msdn_subscription_repo.full_name
@@ -51,7 +51,7 @@ resource "tfe_workspace" "vault_configuration" {
   name         = "my-vault-cluster-configuration"
   organization = var.my_tfc_organization
 
-  terraform_version = "0.14.2"
+  terraform_version = "0.14.3"
 
   vcs_repo {
     identifier = data.terraform_remote_state.github_repositories.outputs.vault_configuration_repo.full_name
