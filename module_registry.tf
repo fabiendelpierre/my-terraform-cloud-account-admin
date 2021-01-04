2166cbbb99ffd838a07943f125bcc343a6ce03f2
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2021-01-04 15:58:33 -0500

    add key vault module to registry

--- module_registry.tf
+++ module_registry.tf
@@ -6,3 +6,12 @@ resource "tfe_registry_module" "virtual_network" {
     oauth_token_id     = data.tfe_oauth_client.my_oauth_client.oauth_token_id
   }
 }
+
+resource "tfe_registry_module" "key_vault" {
+  provider = tfe.module_registry
+  vcs_repo {
+    display_identifier = data.terraform_remote_state.github_repositories.outputs.module_azurerm_keyvault_repo.full_name
+    identifier         = data.terraform_remote_state.github_repositories.outputs.module_azurerm_keyvault_repo.full_name
+    oauth_token_id     = data.tfe_oauth_client.my_oauth_client.oauth_token_id
+  }
+}
