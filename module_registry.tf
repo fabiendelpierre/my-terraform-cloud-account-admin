f99089c85677b3e56caa3c6da21bcabf2ed518ba
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2021-01-11 12:05:45 -0500

    import hashi corp vault module into registry

--- module_registry.tf
+++ module_registry.tf
@@ -24,3 +24,12 @@ resource "tfe_registry_module" "storage_account" {
     oauth_token_id     = data.tfe_oauth_client.my_oauth_client.oauth_token_id
   }
 }
+
+resource "tfe_registry_module" "hashicorp_vault" {
+  provider = tfe.module_registry
+  vcs_repo {
+    display_identifier = data.terraform_remote_state.github_repositories.outputs.module_azurerm_hashicorpvault_repo.full_name
+    identifier         = data.terraform_remote_state.github_repositories.outputs.module_azurerm_hashicorpvault_repo.full_name
+    oauth_token_id     = data.tfe_oauth_client.my_oauth_client.oauth_token_id
+  }
+}
